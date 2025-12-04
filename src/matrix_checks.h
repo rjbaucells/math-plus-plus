7eaec52bb319c5090af48d363f1d1711429a123f
Ricardo Baucells <ricardo@baucells.net>
2025-12-03 22:44:22 -0500

    Moved entire matrix.h to seperate files

--- src/matrix_checks.h
+++ src/matrix_checks.h
@@ -1,2 +1,379 @@
 #pragma once
+#include "matrix.h"
 
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isRowEchelon(bool pivotMustBeOne) const {
+    int lastPivotColumn = -1;
+
+    for (int r = 0; r < ROWS; r++) {
+        bool foundNonZero = false;
+
+        for (int c = 0; c < COLUMNS; c++) {
+            // this is a pivot
+            if (!foundNonZero && !compare(data[c][r], 0)) {
+                // this is to the left of the last pivot
+                if (c < lastPivotColumn)
+                    return false;
+
+                // the pivot must be one and it isn't
+                if (!compare(data[c][r], 1) && pivotMustBeOne)
+                    return false;
+
+                lastPivotColumn = c;
+            }
+
+            if (!compare(data[c][r], 0))
+                foundNonZero = true;
+        }
+
+        // this entire row was zeros, and this wasn't the last row
+        if (!foundNonZero || r != ROWS - 1) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isReducedRowEchelon() const {
+    int lastPivotColumn = -1;
+
+    for (int r = 0; r < ROWS; r++) {
+        bool foundNonZero = false;
+
+        for (int c = 0; c < COLUMNS; c++) {
+            // this is a pivot
+            if (!foundNonZero && !compare(data[c][r], 0)) {
+                // this is to the left of the last pivot
+                if (c < lastPivotColumn)
+                    return false;
+
+                // the pivot isn't 1
+                if (!compare(data[c][r], 1))
+                    return false;
+
+                // check that no other number in that column is a nonzero value
+                for (int i = 0; i < ROWS; i++) {
+                    if (!compare(data[c][i], 0) && !compare(data[c][i], data[c][r]))
+                        return false;
+                }
+
+                lastPivotColumn = c;
+            }
+
+            if (!compare(data[c][r], 0))
+                foundNonZero = true;
+        }
+
+        // this entire row was zeros, and this wasn't the last row
+        if (!foundNonZero || r != ROWS - 1) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isSymmetrical() const requires (isSquare) {
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = 0; r < ROWS; r++) {
+            if (!compare(data[c][r], data[r][c]))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isSkewSymmetrical() const requires (isSquare) {
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = 0; r < ROWS; r++) {
+            if (!compare(data[c][r], -data[r][c]))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isHermitian() const requires (isSquare) {
+    if constexpr (!isComplex)
+        return isSymmetrical();
+
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = 0; r < ROWS; r++) {
+            if (!compare(data[c][r], std::conj(data[r][c]))) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isSkewHermitian() const requires (isSquare) {
+    if constexpr (!isComplex)
+        return isSkewSymmetrical();
+
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = 0; r < ROWS; r++) {
+            if (!compare(data[c][r], -std::conj(data[r][c])))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isPositiveDefinite() const requires (isSquare) {
+    // TODO:
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isPositiveSemiDefinite() const requires (isSquare) {
+    // TODO:
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isNegativeDefinite() const requires (isSquare) {
+    // TODO:
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isNegativeSemiDefinite() const requires (isSquare) {
+    // TODO:
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isUnitary() const requires (isSquare) {
+    // TODO:
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isSpecialUnitary() const requires (isSquare) {
+    // TODO:
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isOrthogonal() const requires (!isComplex && isSquare) {
+    // TODO:
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isSpecialOrthogonal() const requires (!isComplex && isSquare) {
+    // TODO:
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isUpperTriangleMatrix() const requires (isSquare) {
+    for (int c = 0; c < COLUMNS; c++) {
+        // dont worry about out of bounds, loop wont even run if c + 1 is too big
+        for (int r = c + 1; r < ROWS; r++) {
+            if (!compare(data[c][r], 0))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isLowerTriangleMatrix() const requires (isSquare) {
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = 0; r < c; r++) {
+            if (!compare(data[c][r], 0))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isDiagonalMatrix() const requires (isSquare) {
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = 0; r < ROWS; r++) {
+            if (c == r)
+                continue;
+
+            if (!compare(data[c][r], 0))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isUpperUnitriangularMatrix() const requires (isSquare) {
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = c; r < ROWS; r++) {
+            if (c == r) {
+                if (!compare(data[c][r], 1)) {
+                    return false;
+                }
+            }
+            else if (!compare(data[c][r], 0)) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isLowerUnitriangularMatrix() const requires (isSquare) {
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = 0; r <= c; r++) {
+            if (c == r) {
+                if (!compare(data[c][r], 1)) {
+                    return false;
+                }
+            }
+            else if (!compare(data[c][r], 0))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isStrictlyUpperTriangularMatrix() const requires (isSquare) {
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = c; r < ROWS; r++) {
+            if (!compare(data[c][r], 0)) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isStrictlyLowerTriangularMatrix() const requires (isSquare) {
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = 0; r <= c; r++) {
+            if (!compare(data[c][r], 0))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isFrobeniusMatrix() const requires (isSquare) {
+    int columnWithNonZero = -1;
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = 0; r < ROWS; r++) {
+            // not a one on diagonal
+            if (c == r && !compare(data[c][r], 1)) {
+                return false;
+            }
+            if (!compare(data[c][r], 0)) {
+                // its above
+                if (r <= c)
+                    return false;
+
+                // too many columns with nonzero
+                if (columnWithNonZero != c)
+                    return false;
+
+                columnWithNonZero = c;
+            }
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isUpperHessenberg() const requires (isSquare) {
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = c + 2; r < ROWS; r++) {
+            if (!compare(data[c][r], 0))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isUnreducedUpperHessenberg() const requires (isSquare) {
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = c + 1; r < ROWS; r++) {
+            // subdiagonal
+            if (r == c + 1) {
+                if (compare(data[c][r], 0)) {
+                    return false;
+                }
+            }
+            else {
+                if (!compare(data[c][r], 0)) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isLowerHessenberg() const requires (isSquare) {
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = c - 2; r >= 0; r--) {
+            if (!compare(data[c][r], 0))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isUnreducedLowerHessenberg() const requires (isSquare) {
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = c - 1; r >= 0; r--) {
+            // supra-diagonal
+            if (r == c - 1) {
+                if (compare(data[c][r], 0)) {
+                    return false;
+                }
+            }
+            else {
+                if (!compare(data[c][r], 0)) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+bool Matrix<COLUMNS, ROWS, T>::isTridiagonal() const requires (isSquare) {
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = 0; r < ROWS; r++) {
+            // diagonal, subdiagonal, supradiagonal
+            if (c == r || c == r + 1 || c == r - 1)
+                continue;
+
+            if (!compare(data[c][r], 0))
+                return false;
+        }
+    }
+
+    return true;
+}
---
+++ src/matrix_decompositions.h
@@ -0,0 +1,336 @@
+#pragma once
+#include "matrix.h"
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+Matrix<COLUMNS, ROWS, T>::template LUPDecomposition<Matrix<std::min(ROWS, COLUMNS), ROWS, T>, Matrix<COLUMNS, std::min(ROWS, COLUMNS), T>, Matrix<ROWS, ROWS, T>> Matrix<COLUMNS, ROWS, T>::lupDecomposition() const {
+    Matrix<std::min(ROWS, COLUMNS), ROWS, T> l = Matrix<ROWS, ROWS, T>::identity();
+    Matrix<COLUMNS, std::min(ROWS, COLUMNS), T> u = *this;
+    Matrix<ROWS, ROWS, T> p = Matrix<ROWS, ROWS, T>::identity();
+
+    // use std::min so we never access the pivot that's out of the matrix
+    for (int c = 0; c < std::min(ROWS, COLUMNS); c++) {
+        // handle row swaps
+        {
+            int rowIndex = -1;
+            const T pivot = u[c][c];
+            T rowValue = std::abs(pivot);
+
+            // iterate through rows of this column. Looking for the biggest boi
+            for (int r = c + 1; r < ROWS; r++) {
+                T curValue = std::abs(u[c][r]);
+
+                if (curValue > rowValue) {
+                    rowIndex = r;
+                    rowValue = curValue;
+                }
+            }
+
+            // we found nothing but we needed to find something
+            if (rowIndex == -1 && compare(pivot, 0)) {
+                throw std::runtime_error("Cannot LUP decompose singular matrix");
+            }
+
+            if (rowIndex != -1) {
+                // swap u and p rows like normal
+                u.swapRows(c, rowIndex);
+                p.swapRows(c, rowIndex);
+
+                // swap rows of l before column c
+                for (int i = 0; i < c; ++i) {
+                    T temp = l[i][c];
+                    l[i][c] = l[i][rowIndex];
+                    l[i][rowIndex] = temp;
+                }
+            }
+        }
+
+        T pivot = u[c][c];
+
+        // iterate through things beneath that pivot in the matrix
+        for (int r = c + 1; r < ROWS; r++) {
+            T val = u[c][r];
+
+            T multiplierToPivotRow = val / pivot;
+
+            l[c][r] = multiplierToPivotRow;
+
+            // do this row minus other row times multiplier
+            for (int i = c; i < COLUMNS; i++) {
+                u[i][r] += -multiplierToPivotRow * u[i][c];
+            }
+        }
+    }
+
+    return {l, u, p};
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+Matrix<COLUMNS, ROWS, T>::template LUPQDecomposition<Matrix<std::min(ROWS, COLUMNS), ROWS, T>, Matrix<COLUMNS, std::min(ROWS, COLUMNS), T>, Matrix<ROWS, ROWS, T>, Matrix<COLUMNS, COLUMNS, T>> Matrix<COLUMNS, ROWS, T>::lupqDecomposition() const {
+    Matrix<std::min(ROWS, COLUMNS), ROWS, T> l = Matrix<ROWS, ROWS, T>::identity();
+    Matrix<COLUMNS, std::min(ROWS, COLUMNS), T> u = *this;
+    Matrix<ROWS, ROWS, T> p = Matrix<ROWS, ROWS, T>::identity();
+    Matrix<COLUMNS, COLUMNS, T> q = Matrix<COLUMNS, COLUMNS, T>::identity();
+
+    // use std::min so we never access the pivot that's out of the matrix
+    for (int c = 0; c < std::min(ROWS, COLUMNS); c++) {
+        // handle row AND column swaps
+        {
+            int rowIndex = -1;
+            int columnIndex = -1;
+
+            const T pivot = u[c][c];
+
+            T maxValue = std::abs(pivot);
+
+            for (int i = c; i < COLUMNS; i++) {
+                for (int j = c; j < ROWS; j++) {
+                    T curValue = std::abs(u[i][j]);
+
+                    if (curValue > maxValue) {
+                        maxValue = curValue;
+                        columnIndex = i;
+                        rowIndex = j;
+                    }
+                }
+            }
+
+            if (rowIndex == -1 && columnIndex == -1 && compare(pivot, 0)) {
+                throw std::runtime_error("Cannot LUPQ decompose singular matrix");
+            }
+
+            if (rowIndex != c) {
+                u.swapRows(c, rowIndex);
+                p.swapRows(c, rowIndex);
+                // swap rows of L before column c
+                for (int i = 0; i < c; ++i) {
+                    T temp = l[i][c];
+                    l[i][c] = l[i][rowIndex];
+                    l[i][rowIndex] = temp;
+                }
+            }
+
+            if (columnIndex != c) {
+                u.swapColumns(c, columnIndex);
+                q.swapColumns(c, columnIndex);
+            }
+        }
+
+        T pivot = u[c][c];
+
+        // iterate through things beneath that pivot in the matrix
+        for (int r = c + 1; r < ROWS; r++) {
+            T val = u[c][r];
+
+            T multiplierToPivotRow = val / pivot;
+
+            l[c][r] = multiplierToPivotRow;
+
+            // do this row minus other row times multiplier
+            for (int i = c; i < COLUMNS; i++) {
+                u[i][r] += -multiplierToPivotRow * u[i][c];
+            }
+        }
+    }
+
+    return {l, u, p, q};
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+Matrix<COLUMNS, ROWS, T>::template LUDecomposition<Matrix<std::min(ROWS, COLUMNS), ROWS, T>, Matrix<COLUMNS, std::min(ROWS, COLUMNS), T>> Matrix<COLUMNS, ROWS, T>::luDecomposition() const {
+    Matrix<std::min(ROWS, COLUMNS), ROWS, T> l = Matrix<ROWS, ROWS, T>::identity();
+    Matrix<COLUMNS, std::min(ROWS, COLUMNS), T> u = *this;
+
+    // use std::min so we never access the pivot that's out of the matrix
+    for (int c = 0; c < std::min(ROWS, COLUMNS); c++) {
+        T pivot = u[c][c];
+
+        if (compare(pivot, 0))
+            throw std::runtime_error("Cannot LU decompose matrix due to zero pivot, try LUP or LUPQ");
+
+        // iterate through things beneath that pivot in the matrix
+        for (int r = c + 1; r < ROWS; r++) {
+            T val = u[c][r];
+
+            T multiplierToPivotRow = val / pivot;
+
+            l[c][r] = multiplierToPivotRow;
+
+            // do this row minus other row times multiplier
+            for (int i = c; i < COLUMNS; i++) {
+                u[i][r] += -multiplierToPivotRow * u[i][c];
+            }
+        }
+    }
+
+    return {l, u};
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+Matrix<COLUMNS, ROWS, T>::template LDUDecomposition<Matrix<std::min(ROWS, COLUMNS), ROWS, T>, Matrix<std::min(ROWS, COLUMNS), std::min(ROWS, COLUMNS), T>, Matrix<COLUMNS, std::min(ROWS, COLUMNS), T>> Matrix<COLUMNS, ROWS, T>::lduDecomposition() const {
+    Matrix<std::min(ROWS, COLUMNS), ROWS, T> l = Matrix<ROWS, ROWS, T>::identity();
+    Matrix<std::min(ROWS, COLUMNS), std::min(ROWS, COLUMNS), T> d = Matrix<std::min(ROWS, COLUMNS), std::min(ROWS, COLUMNS), T>::identity();
+    Matrix<COLUMNS, std::min(ROWS, COLUMNS), T> u = *this;
+
+    // use std::min so we never access the pivot that's out of the matrix
+    for (int c = 0; c < std::min(ROWS, COLUMNS); c++) {
+        T pivot = u[c][c];
+
+        if (compare(pivot, 0))
+            throw std::runtime_error("Cannot LDU decompose matrix due to zero pivot");
+
+        // iterate through things beneath that pivot in the matrix
+        for (int r = c + 1; r < ROWS; r++) {
+            T val = u[c][r];
+
+            T multiplierToPivotRow = val / pivot;
+
+            l[c][r] = multiplierToPivotRow;
+
+            // do this row minus other row times multiplier
+            for (int i = c; i < COLUMNS; i++) {
+                u[i][r] += -multiplierToPivotRow * u[i][c];
+            }
+        }
+    }
+
+    for (int c = 0; c < COLUMNS; c++) {
+        d[c][c] = u[c][c];
+
+        for (int r = 0; r < ROWS; r++) {
+            u[c][r] /= d[c][c];
+        }
+    }
+
+    return {l, d, u};
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+Matrix<COLUMNS, ROWS, T>::template CholeskyDecomposition<Matrix<COLUMNS, ROWS, T>, Matrix<ROWS, COLUMNS, T>> Matrix<COLUMNS, ROWS, T>::choleskyDecomposition(const bool allowPositiveSemiDefinite) const requires (isSquare) {
+    if (!isHermitian()) {
+        throw std::runtime_error("Cannot find Cholesky Decomposition of non hermitian/symmetric matrix");
+    }
+
+    Matrix<COLUMNS, ROWS, T> l;
+
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = 0; r <= c; r++) {
+            if (r == c) {
+                T value = data[c][c];
+
+                for (int k = 0; k < c; k++) {
+                    // TODO: Find a way to replace this if
+                    if constexpr (isComplex) {
+                        value -= l[k][c] * std::conj(l[k][c]);
+                    }
+                    else {
+                        value -= std::pow(l[k][c], 2);
+                    }
+                }
+
+                if (value < 0) {
+                    throw std::runtime_error("Cannot cholesky decompose non positive definite matrix");
+                }
+
+                if (compare(value, 0) && !allowPositiveSemiDefinite) {
+                    throw std::runtime_error("Cannot cholesky decompose non semi-positive definite matrix");
+                }
+
+                l[c][c] = std::sqrt(value);
+            }
+            else if (r > c) {
+                T value = data[c][r];
+
+                for (int k = 0; k < c; k++) {
+                    value -= l[k][r] * l[k][c];
+                }
+
+                l[c][r] = value / l[c][c];
+            }
+        }
+    }
+
+    return {l, l.conjugateTranspose()};
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+Matrix<COLUMNS, ROWS, T>::template LDLDecomposition<Matrix<COLUMNS, ROWS, T>, Matrix<COLUMNS, ROWS, T>, Matrix<ROWS, COLUMNS, T>> Matrix<COLUMNS, ROWS, T>::ldlDecomposition() const requires (isSquare) {
+    Matrix<COLUMNS, ROWS, T> l;
+    Matrix<COLUMNS, ROWS, T> d;
+
+    for (int c = 0; c < COLUMNS; c++) {
+        for (int r = 0; r <= c; r++) {
+            if constexpr (!isComplex) {
+                if (r == c) {
+                    T value = data[c][c];
+
+                    for (int k = 0; k < c; k++) {
+                        value -= std::pow(l[k][c], 2) * d[k][k];
+                    }
+
+                    d[c][c] = value;
+                }
+                else if (r > c) {
+                    T value = data[c][r];
+
+                    for (int k = 0; k < c; k++) {
+                        value -= l[k][r] * l[k][c] * d[k][k];
+                    }
+
+                    l[c][r] = (1 / d[c][c]) * value;
+                }
+            }
+            else {
+                if (r == c) {
+                    T value = data[c][c];
+
+                    for (int k = 0; k < c; k++) {
+                        T complexConjugate = l[k][c];
+                        complexConjugate.imag *= -1;
+                        value -= l[k][c] * complexConjugate * d[k][k];
+                    }
+
+                    d[c][c] = value;
+                }
+                else if (r > c) {
+                    T value = data[c][r];
+
+                    for (int k = 0; k < c; k++) {
+                        T complexConjugate = l[k][c];
+                        complexConjugate.imag *= -1;
+
+                        value -= l[k][r] * complexConjugate * d[k][k];
+                    }
+
+                    l[c][r] = (1 / d[c][c]) * value;
+                }
+            }
+        }
+    }
+
+    return {l, d, l.conjugateTranspose()};
+}
+
+template<int COLUMNS, int ROWS, is_scalar_v T>
+Matrix<COLUMNS, ROWS, T>::template QRDecomposition<Matrix<ROWS, ROWS, T>, Matrix<COLUMNS, ROWS, T>> Matrix<COLUMNS, ROWS, T>::qrDecomposition() const requires (isSquare) {
+    std::array<Vector<ROWS>, COLUMNS> a = getColumnVectors();
+    std::array<Vector<ROWS>, COLUMNS> u = {};
+    Matrix<ROWS, ROWS, T> q;
+
+    for (int k = 0; k < COLUMNS; k++) {
+        u[k] = a[k];
+
+        for (int j = 0; j < k; j++) {
+            u[k] -= u[j].projection(a[k]);
+        }
+
+        T uMagnitude = u[k].magnitude();
+
+        for (int i = 0; i < ROWS; i++) {
+            q[k][i] = u[k][i] / uMagnitude;
+        }
+    }
+
+    Matrix<COLUMNS, ROWS, T> r = q.transpose() * *this;
+
+    return {q, r};
+}
